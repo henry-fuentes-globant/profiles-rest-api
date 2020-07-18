ee7655048d2728a35ce19bdb9c0898c51e02be37
Henry Fuentes <henry.fuentes@smiledirectclub.com>
2020-07-18 16:13:06 -0500

    fix path

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/henry-fuentes-globant/profiles-rest-api.git'
 
-PROJECT_BASE_PATH='/c/demos/profiles-rest-api'
+PROJECT_BASE_PATH='/c/demos/profiles-rest-api-2'
 
 echo "Installing dependencies..."
 apt-get update
